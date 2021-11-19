a65a14a232cd025814aba64defa7612b06d17a81
Shi Warai <bagerx.original@gmail.com>
2021-11-19 21:36:43 +0300

    Feat: починил скрипт для отображения изменений в гите

--- Сайт/Export_script/bootstrap_export_script.py
+++ Сайт/Export_script/bootstrap_export_script.py
@@ -12,7 +12,7 @@ def getDirsList(path):
     return listdir(path)
 
 def getFilesList(directory):
-    return [path.join(directory,fileName) for fileName in getDirsList(directory) if path.isfile(path.join(directory, fileName))]
+    return [path.join(directory, fileName) for fileName in getDirsList(directory) if path.isfile(path.join(directory, fileName))]
 
 for template in getFilesList(template_path):
     file_data = str()
@@ -36,7 +36,6 @@ for template in getFilesList(template_path):
         first_script.insert_before("{% block scripts %}")
         last_script.insert_after("{% endblock scripts %}")
 
-        file_data = str(soup)
     else:
         soup = BeautifulSoup(file_data, 'html.parser')
 
@@ -51,7 +50,5 @@ for template in getFilesList(template_path):
         first_script.insert_before("{% block scripts %}")
         last_script.insert_after("{% endblock scripts %}")
 
-        file_data = str(soup)
-
-    with open(template, 'w', encoding='utf-8') as file:
-        file.write(file_data)
+    with open('test', 'wb') as file:
+        file.write(soup.prettify("utf-8"))
